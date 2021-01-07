692f72828bed6ced536e403d32dad75c7dcc422c
Joel <j_90@live.co.uk>
2021-01-07 23:13:12 +0000

    adds obj constructor function

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function () {
+  // Customers
+  let customers = [];
+  let index = 0;
+
+  // Object constructor function
+  function Customer(name, img, text) {
+    this.name = name;
+    this.img = img;
+    this.text = text;
+  }
+})();
